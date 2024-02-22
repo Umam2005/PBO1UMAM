00e5ab7626d3818f98bfe4e49b42e6f88ad12da2
Umam2005 <anakmami100205@gmail.com>
2024-02-22 23:04:01 +0700

    codelab

---
+++ coding awal/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String nama, jenisKelamin, tglLahir;
+
+        System.out.print("Masukan Nama: ");
+        nama = input.nextLine();
+        System.out.print("Masukan Jenis Kelamin: ");
+        jenisKelamin = input.nextLine();
+        System.out.print("Masukan Tanggal Lahir: ");
+        tglLahir =input.nextLine();
+
+    }
+}
